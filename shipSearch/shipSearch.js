2cde216f142d5e157dbcbc6fdefcef79527a6277
alsrl441 <128763318+alsrl441@users.noreply.github.com>
2026-04-05 04:02:44 +0000

    update

--- shipSearch/shipSearch.js
+++ shipSearch/shipSearch.js
@@ -320,9 +320,9 @@ function showHistoryDetail(shipIdx, historyIdx) {
             </div>
         </div>
         <div class="history-info-group fade-in">
+            <div class="h-item" style="flex: 1;"><label>인수인계 사항</label><span>${h.handover || '데이터 없음'}</span></div>
             <div class="h-item"><label>근무자</label><span>${h.worker || '미입력'}</span></div>
             <div class="h-item"><label>수화자</label><span>${h.telephonee || '미입력'}</span></div>
-            <div class="h-item" style="flex: 1;"><label>인수인계 사항</label><span>${h.handover || '데이터 없음'}</span></div>
         </div>
     `;
     const pathBox = card.querySelector('.history-path-box');
